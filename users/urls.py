ea1f8e855cec31f0884a7e2cef6ed8c94eb1cca5
MiXaYLo <mehdinuruzade@std.sehir.edu.tr>
2016-12-08 14:19:20 +0300

    software assignment 2

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url, include
+
+from .views import signup
+
+
+urlpatterns = [
+    url(r'^users/register/$', signup),
+    url(r'', include("django.contrib.auth.urls"))
+
+]
